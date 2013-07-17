390828c789d83e6500cefae60bbd573412330258
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-17 14:51:26 -0500

    left off at creating short url

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,10 +6,15 @@ end
 post '/urls' do
   # create a new Url
   p params
-  @shorty = Url.create(params[:url])
+  @shorty = Url.new(long_url: params[:url])
+  @shorty.short_url = Url.get_shorten!(@shorty.long_url)
+  @shorty.save
+
+  erb :show_url
 end
 
 # e.g., /q6bda
 get '/:short_url' do
   # redirect to appropriate "long" URL
+  erb :show_url
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,25 +1,43 @@
 class Url < ActiveRecord::Base
   # Remember to create a migration!
 
-  before_save :ensure_long_url_is_valid
+  #before_save :ensure_long_url_is_valid
 
-protected:
-  def ensure_long_url_is_valid
-    @longs = self.where(long_url: = url.long_url)
+  def get_shorten!(long_url)
+    
+    matched_long_url_arr = self.where('long_url = #{url.long_url}')
 
-    # if long url is in the database already 
-        #then increment the click_counter 
-        # show the short url that already exists
-     # else the long url doesn't exist in the database model Url
-        # create a short url 
-        # check to see if short url already exists
-
-        # create a short url
-
-        # while short exists
-            # create another short url
-        # end
+    if matched_long_url_arr.empty?
+      newLongUrl!(long_url)
+    else
+      oldLongUrl!(long_url)
+    end
   end
+
+  private 
+  # long url is in the database already 
+    def oldLongUrl!(long_url) 
+      url = Url.find_by(long_url: long_url)
+      url.click_counter += 1      #then increment the click_counter 
+      url.short_url               # show the short url that already exists
+    end
+
+# the long url doesn't exist in the database model Url
+    def newLongUrl!(long_url) 
+        
+        # create a short url 
+        short_url = ""
+        'a1'.upto('zzzzz9') do |i|
+          # check to see if short url already exists
+          if Url.find_by(long_url: "/" + short_url).size == 0
+            short_url = "/#{i}"
+            break
+          end
+        end
+        puts short_url
+        
+        self.create
+    end
 end
 
 
