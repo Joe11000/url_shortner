05b65ab33e5d885c8496b297838804520072efcc
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-17 22:51:53 -0500

    working url shortener

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -6,15 +6,19 @@ end
 post '/urls' do
   # create a new Url
   p params
-  @shorty = Url.new(long_url: params[:url])
-  @shorty.short_url = Url.get_shorten!(@shorty.long_url)
+  @shorty = Url.new(params[:url])
+  p @shorty
+  # @shorty.short_url = @shorty.get_shorten!(@shorty.long_url)
+  @shorty.short_url = @shorty.get_shorten!(@shorty.long_url)
   @shorty.save
-
+  # @show = "short"
+  @show = 'short'
   erb :show_url
 end
 
 # e.g., /q6bda
 get '/:short_url' do
   # redirect to appropriate "long" URL
+  @show = "long"
   erb :show_url
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -4,9 +4,10 @@ class Url < ActiveRecord::Base
   #before_save :ensure_long_url_is_valid
 
   def get_shorten!(long_url)
-    
-    matched_long_url_arr = self.where('long_url = #{url.long_url}')
-
+    puts "pie"
+    p long_url 
+    matched_long_url_arr = Url.where("long_url = ?", long_url)
+    p matched_long_url_arr 
     if matched_long_url_arr.empty?
       newLongUrl!(long_url)
     else
@@ -14,29 +15,30 @@ class Url < ActiveRecord::Base
     end
   end
 
-  private 
+  
   # long url is in the database already 
     def oldLongUrl!(long_url) 
-      url = Url.find_by(long_url: long_url)
+      puts "OldLong" 
+      url = Url.find_by("long_url = ?", long_url)
       url.click_counter += 1      #then increment the click_counter 
       url.short_url               # show the short url that already exists
     end
 
 # the long url doesn't exist in the database model Url
     def newLongUrl!(long_url) 
-        
+        puts "NewLong" 
         # create a short url 
         short_url = ""
         'a1'.upto('zzzzz9') do |i|
           # check to see if short url already exists
-          if Url.find_by(long_url: "/" + short_url).size == 0
-            short_url = "/#{i}"
+          i = '/' + i  
+          Url.where("long_url = ?",  i)
+          if Url.where("long_url = ?",  i).size == 0
+            short_url = "#{i}"
             break
           end
         end
-        puts short_url
-        
-        self.create
+        short_url
     end
 end
 
